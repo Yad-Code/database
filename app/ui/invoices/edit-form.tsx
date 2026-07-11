7dcda96b2997a87c90f045ebb07bce1a49e08303
unknown <yad.4322@gmail.com>
2026-07-11 14:17:45 +0300

    authentication

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -128,7 +128,6 @@ export default function EditInvoiceForm({
                 <p className="mt-2 text-sm text-red-500" key={error}>
                   {error}
                 </p>
-                // 
               ))}
           </div>
           
