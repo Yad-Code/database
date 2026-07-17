7843eaad834f6f23b161d978ecb483e257cdd12c
unknown <yad.4322@gmail.com>
2026-07-17 13:53:47 +0300

    send

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -9,11 +9,11 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
-import { createInvoice, State } from "@/app/lib/actions";
+import { createInvoice } from "@/app/lib/actions";
 import { useActionState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState: State = { message: null, error: {} };
+  const initialState = { message: "", errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
 
   return (
